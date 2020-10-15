7f9d6b3a796a8b1ca98e27d8c907a350c7d95b55
sawantrudra <sawantrudra@ymail.com>
2020-10-15 15:55:52 +1000

    commit 102

--- php/CI/application/config/config.php
+++ php/CI/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://104.155.136.167/';
+$config['base_url'] = 'http://104.155.136.167:8000/CI';
 
 /*
 |--------------------------------------------------------------------------
